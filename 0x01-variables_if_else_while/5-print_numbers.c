227952bd538df900df0d83b656c123f016958c74
Ada <aseradane@Asers-MacBook-Pro.local>
2022-06-03 22:59:05 +0300

    base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- base 10 starting from 0
+ * Return: will returns 0                                                       */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
