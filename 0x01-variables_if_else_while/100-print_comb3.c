a9cc2a3d5acf88b53cbfe51693bec6a73f62a30b
Ada <aseradane@Asers-MacBook-Pro.local>
2022-06-03 23:15:50 +0300

    all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- will show all possible different combinations of two digits
+ * Return: will returns 0
+ */
+
+int main(void)
+{
+int i, e;
+i = 48;
+e = 48;
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+
+
+}
+putchar('\n');
+return (0);
+}
