395b92057c7294d75309c345eba93f005cd88ebd
Ada <aseradane@Asers-MacBook-Pro.local>
2022-06-09 22:41:52 +0300

    Function and nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * _isupper - checks if parameter is an uppercase character.                    * @c: input character.
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
-*/
+ */
 int _isupper(int c)
 {
   if (c >= 65 && c <= 90)
